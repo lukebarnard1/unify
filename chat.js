c09a85b6a73d2b9557030bdba7dd6ab3ad100358
lukebarnard <luke.barnard99@gmail.com>
2014-02-22 17:06:13 +0000

    Further changes to begin creating chat page feature. I think I will branch before making further changes.

---
+++ chat.js
@@ -0,0 +1,16 @@
+
+
+/**
+ * Add a conversation with someone to the conversations box.
+ */
+function add_conversation(user_id) {
+
+}
+
+/**
+ * Display a dialogue of the users friends allowing them to
+ * select one of them, whose id is then returned.
+ */
+function select_friend() {
+
+}
