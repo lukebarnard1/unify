aeb0bb6ec5233bc934b28e7d094f7c02a0fa08f9
lukebarnard <luke.barnard99@gmail.com>
2014-03-23 16:01:05 +0000

    Back end for adding a new group with the user in it is complete. Now need to add functionality to add others to the same group, this will take much longer.

---
+++ script/user_group/add.php
@@ -0,0 +1,28 @@
+<?php
+	//Add a new group, given its name and also add the user who's created it to the group
+
+	include "../util/session.php";
+	include_once("../util/mysql.php");
+	include_once("../util/status.php");
+	include_once("../util/redirect.php");
+
+
+	$group_name = $_GET["group_name"];
+
+	$dao = new DAO(false);
+
+	$group = DataObject::create($dao,"user_group",array("group_name"=>$group_name));
+	$group->commit();
+
+	if ($group) {
+		$grouping = DataObject::create($dao,"grouping",array("group_id"=>$group->group_id,"user_id"=>$user->user_id));
+		$grouping->commit();
+		if ($grouping) {
+			redirect("/group/".$grouping->group_id);
+		} else {
+			redirect("/?m=0");
+		}
+	} else {
+		redirect("/?m=0");
+	}
+?>
