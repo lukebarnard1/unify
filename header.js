039a7f72ea0aefe8187a93e0ddad5f7d7908251f
lukebarnard <luke.barnard99@gmail.com>
2014-03-23 17:35:39 +0000

    Added the ability for users to create their own groups. They are redirected correctly. Next up, I will enable adding other people to groups (backend first).

--- header.js
+++ header.js
@@ -2,6 +2,7 @@
 
 view_nav_button_chat = new Template("view/nav_button_chat.html");
 view_notification = new Template("view/notification.html");
+view_group = new Template("view/group.html");
 
 modifier_notifications = function (data, input_data) {
 	n = data.length;
@@ -26,12 +27,23 @@ function update_unread_messages() {
 	ajax_request(id("nav_button_chat"), false, view_nav_button_chat, modifier_relay, "script/chat_msg/number_unseen.php");
 }
 
-function load_notifications_callback() {
-	notifications = id("notifications");
+function load_notifications() {
+	ajax_request(notifications, false, view_notification, modifier_notifications, "script/notification/get.php",null);
 }
 
-function load_notifications() {
-	ajax_request(notifications, false, view_notification, modifier_notifications, "script/notification/get.php",null,load_notifications_callback);
+function add_group(event) {
+	group_name = event.target.innerText;
+	event.preventDefault();
+
+	window.location.replace("/script/user_group/add.php?group_name="+group_name);
+}
+
+function load_groups() {
+	callback = function () {
+		groups.innerHTML += "<div style=\"background-color:#fff;color:#000\" onkeydown=\"if(event.keyCode==13)add_group(event);\" onblur=\"this.innerHTML=(this.innerHTML==''?'New group':this.innerHTML)\" onclick=\"this.contentEditable='true';this.innerHTML=(this.innerHTML=='New group')?'':this.innerHTML;\">New group</div>";
+	}
+
+	ajax_request(groups, false, view_group, modifier_relay, "script/user_group/get.php",null,callback);
 }
 
 function load() {
@@ -39,6 +51,7 @@ function load() {
 	setInterval(update_unread_messages, 3000);
 	load_notifications();
 	setInterval(load_notifications, 3000);
+	load_groups();
 }
 
 window.addEventListener("load",load);
