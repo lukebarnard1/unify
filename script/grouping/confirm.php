9788c3b6b67c9ab91b7b9b9691c24463752ce6f9
lukebarnard <luke.barnard99@gmail.com>
2014-03-28 16:27:01 +0000

    Fixed error messages for users already in group and for users not asked to be in group.

--- script/grouping/confirm.php
+++ script/grouping/confirm.php
@@ -1,28 +1,34 @@
 <?php
-	//Confirm that this user logged in wants to join the group
-	include "../util/session.php";
-	include "../util/redirect.php";
-	include_once("../util/mysql.php");
+//Confirm that this user logged in wants to join the group
+include "../util/session.php";
+include "../util/redirect.php";
+include_once("../util/mysql.php");
 
-	$group_id = $_GET["group_id"];
+$group_id = $_GET["group_id"];
 
-	$dao = new DAO(false);
+$new_values = array("group_id"=>$group_id,"user_id"=>$user->user_id);
 
+$dao = new DAO(false);
+
+//Check if the user has already been added:
+$already_grouped = DataObject::select_one($dao, "grouping", array("grouping_id"), $new_values);
+
+if ($already_grouped == NULL) {
+	$grouping = DataObject::create($dao, "grouping", $new_values);
 	$request = DataObject::select_one($dao, "grouping_request", array("gr_id","group_id","user_id"),array("group_id"=>$group_id,"user_id"=>$user->user_id));
 
 	if ($request != NULL) {
 		$request->delete();//Delete the request from the database
 
-		$grouping = DataObject::create($dao, "grouping", array("group_id"=>$group_id,"user_id"=>$user->user_id));
-		$grouping->commit();
-
-		if ($grouping != NULL) {
+		if ($grouping->commit()) {
 			redirect("/group/$group_id");//Send them to the new group!
 		} else {
-			redirect("/?m=0");
+			redirect("/?m=11");
 		}
 	} else {
-		redirect("/?m=0");
+		redirect("/?m=13"); //You have not been asked to join this group
 	}
-
+} else {
+	redirect("/?group_id=".$group_id."&m=14"); //You are already in this group... See!
+}
 ?>
