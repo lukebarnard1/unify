59e0a48e19b8f686f8c2fe9c33bfa11e9807f2d7
lukebarnard <luke.barnard99@gmail.com>
2014-04-05 16:38:16 +0100

    You now cannot request someone to join who has already been requested.

--- script/grouping/request.php
+++ script/grouping/request.php
@@ -15,21 +15,29 @@
 
 	if ($group != NULL) {
 		if ($member != NULL) {
-			$body = "<p>Hello ".$member->user_name.",</p>
-			<p>".$user->user_name." has asked you to join the group \"".$group->group_name."\".
-				If you would like to join, please click on this link: 
-				<a href=\"".$SITE_URL."script/grouping/confirm.php?group_id=".$group_id."\">Click here to join</a>.</p>
-			<p>Best Wishes,<br>The Unify Team</p>";
 
-			$request = DataObject::create($dao,"grouping_request", array("group_id"=>$group_id,"user_id"=>$user_id));
-			$request->commit();//Put the request in the database. So long as this is here, the user can accept (only when logged in)
+			if (NULL == DataObject::select_one($dao, "grouping_request", 
+				array("gr_id"), 
+				array("group_id"=>$group_id,"user_id"=>$user_id))) {
 
-			mail_message($member->user_email, "Group Join Request", $body);
-			echo Status::json(0,"Request sent :)");
+				$body = "<p>Hello ".$member->user_name.",</p>
+				<p>".$user->user_name." has asked you to join the group \"".$group->group_name."\".
+					If you would like to join, please click on this link: 
+					<a href=\"".$SITE_URL."script/grouping/confirm.php?group_id=".$group_id."\">Click here to join</a>.</p>
+				<p>Best Wishes,<br>The Unify Team</p>";
+
+				$request = DataObject::create($dao,"grouping_request", array("group_id"=>$group_id,"user_id"=>$user_id));
+				$request->commit();//Put the request in the database. So long as this is here, the user can accept (only when logged in)
+
+				mail_message($member->user_email, "Group Join Request", $body);
+				echo Status::json(0,"Request sent :)");
+			} else {
+				echo Status::json(3,"Member has already been requested to join");
+			}
 		} else {
-			echo Status::json(1,"Member not found.");
+			echo Status::json(1,"Member not found");
 		}
 	} else {
-		echo Status::json(2,"Group not found.");
+		echo Status::json(2,"Group not found");
 	}
 ?>
