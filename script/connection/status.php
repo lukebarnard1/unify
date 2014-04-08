3faa99662f6c0761b993085f8cbbe5a11a6d2797
lukebarnard <luke.barnard99@gmail.com>
2014-04-08 13:34:19 +0100

    Bug fix and this status has now been tested and gives fairly good status information to the user

--- script/connection/status.php
+++ script/connection/status.php
@@ -2,6 +2,7 @@
 	//Determine the status of a connection: requested/connected/non existant
 
 	include "../util/session.php";
+	include "../util/session_var.php";
 	include_once("../util/mysql.php");
 	include_once("../util/status.php");
 
@@ -11,6 +12,10 @@
 		"user_id1" => $user->user_id,
 		"user_id2" => $selected_user->user_id
 	);
+	$connection_rev = array(
+		"user_id2" => $user->user_id,
+		"user_id1" => $selected_user->user_id
+	);
 
 	//Has it been requested?
 
@@ -18,7 +23,7 @@
 	if ($request == NULL) {
 		//Check if they are friends
 
-		$friendship = DataObject::select_one($dao, "connection", array("connection_id"), $connection);
+		$friendship = DataObject::select_one($dao, "connection", array("connection_id"), $connection_rev);
 		if ($friendship != NULL) {
 			echo Status::json(0, "Unification complete: <a href=\"javascript:;\" onclick=\"location.reload()\">refresh page?</a>");
 		} else {
